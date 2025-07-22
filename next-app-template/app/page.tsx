84b6e1f45aaaf532451de61c380cea9f1da1c175
Ibraheem <ebrahimqa99@gmail.com>
2025-07-22 04:04:20 +0300

    e

--- next-app-template/app/page.tsx
+++ next-app-template/app/page.tsx
@@ -4,20 +4,19 @@ import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
 import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <span>Make&nbsp;</span>
+        <span>beautiful&nbsp;</span>
         <br />
-        <span className={title()}>
+        <span>
           websites regardless of your design experience.
         </span>
-        <div className={subtitle({ class: "mt-4" })}>
+        <div>
           Beautiful, fast and modern React UI library.
         </div>
       </div>
