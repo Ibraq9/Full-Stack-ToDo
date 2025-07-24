de6dffea7643be490556fb573dc7d64f2c2fccfa
Ibraheem <ebrahimqa99@gmail.com>
2025-07-24 04:29:56 +0300

    e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-orange-50 dark:bg-gray-900 antialiased `}
       ><StackProvider app={stackServerApp}><StackTheme>
         <ModalProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
